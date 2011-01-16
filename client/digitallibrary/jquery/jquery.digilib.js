ecc0d461b53efbb75d7234aa81f9d4c2de765dc7
hertzhaft <hertzhaft@localhost>
2011-01-16 14:05:17 +0100

    highlight hovered buttons

---
+++ client/digitallibrary/jquery/jquery.digilib.js
@@ -0,0 +1,358 @@
+/*
+ * digilib jQuery plugin
+ *
+ */
+
+(function($) {
+    var buttons = {
+        reference : {
+            onclick : "javascript:getRefWin()",
+            tooltip : "get a reference URL",
+            img : "reference.png"
+            },
+        zoomin : {
+            onclick : "javascript:dl.zoomBy(1.4)",
+            tooltip : "zoom in",
+            img : "zoom-in.png"
+            },
+        zoomout : {
+            onclick : "javascript:zoomBy(0.7)",
+            tooltip : "zoom out",
+            img : "zoom-out.png"
+            },
+        zoomarea : {
+            onclick : "javascript:zoomArea()",
+            tooltip : "zoom area",
+            img : "zoom-area.png"
+            },
+        zoomfull : {
+            onclick : "javascript:zoomFullpage()",
+            tooltip : "view the whole image",
+            img : "zoom-full.png"
+            },
+        pagewidth : {
+            onclick : "javascript:zoomFullpage('width')",
+            tooltip : "page width",
+            img : "pagewidth.png"
+            },
+        back : {
+            onclick : "javascript:gotoPage('-1')",
+            tooltip : "goto previous image",
+            img : "back.png"
+            },
+        fwd : {
+            onclick : "javascript:gotoPage('+1')",
+            tooltip : "goto next image",
+            img : "fwd.png"
+            },
+        page : {
+            onclick : "javascript:gotoPageWin()",
+            tooltip : "specify image",
+            img : "page.png"
+            },
+        bird : {
+            onclick : "javascript:toggleBirdDiv()",
+            tooltip : "show bird's eye view",
+            img : "birds-eye.png"
+            },
+        help : {
+            onclick : "javascript:toggleAboutDiv()",
+            tooltip : "about Digilib",
+            img : "help.png"
+            },
+        reset : {
+            onclick : "javascript:resetImage()",
+            tooltip : "reset image",
+            img : "reset.png"
+            },
+        mark : {
+            onclick : "javascript:setMark()",
+            tooltip : "set a mark",
+            img : "mark.png"
+            },
+        delmark : {
+            onclick : "javascript:removeMark()",
+            tooltip : "delete the last mark",
+            img : "delmark.png"
+            },
+        hmir : {
+            onclick : "javascript:mirror('h')",
+            tooltip : "mirror horizontally",
+            img : "mirror-horizontal.png"
+            },
+        vmir : {
+            onclick : "javascript:mirror('v')",
+            tooltip : "mirror vertically",
+            img : "mirror-vertical.png"
+            },
+        rot : {
+            onclick : "javascript:setParamWin('rot', 'Rotate (0..360) clockwise')",
+            tooltip : "rotate image",
+            img : "rotate.png"
+            },
+        brgt : {
+            onclick : "javascript:setParamWin('brgt', 'Brightness (-255..255)')",
+            tooltip : "set brightness",
+            img : "brightness.png"
+            },
+        cont : {
+            onclick : "javascript:setParamWin('cont', 'Contrast (0..8)')",
+            tooltip : "set contrast",
+            img : "contrast.png"
+            },
+        rgb : {
+            onclick : "javascript:setParamWin('rgb', '...')",
+            tooltip : "set rgb values",
+            img : "rgb.png"
+            },
+        quality : {
+            onclick : "javascript:setQualityWin('Quality (0..2)')",
+            tooltip : "set image quality",
+            img : "quality.png"
+            },
+        size : {
+            onclick : "javascript:toggleSizeMenu()",
+            tooltip : "set page size",
+            img : "size.png"
+            },
+        calibrationx : {
+            onclick : "javascript:calibrate('x')",
+            tooltip : "calibrate screen x-ratio",
+            img : "calibration-x.png"
+            },
+        scale : {
+            onclick : "javascript:toggleScaleMenu()",
+            tooltip : "change image scale",
+            img : "original-size.png"
+            },
+        options : {
+            onclick : "javascript:toggleOptionDiv()",
+            tooltip : "hide options",
+            img : "options.png"
+            },
+        SEP : {
+            img : "sep.png"
+            }
+        };
+        
+    var defaults = {
+        // base URL to Scaler servlet
+        'scalerBaseUrl' : 'http://digilib.mpiwg-berlin.mpg.de/digitallibrary/servlet/Scaler',
+        // list of Scaler parameters
+            'scalerParamNames' : ['fn','pn','dw','dh','ww','wh','wx','wy','ws','mo',
+                                  'rot','cont','brgt','rgbm','rgba','ddpi','ddpix','ddpiy'],
+        // mode of operation. 
+        // fullscreen: takes parameters from page URL, keeps state in page URL
+        // embedded: takes parameters from Javascript options, keeps state inside object 
+        'interactionMode' : 'fullscreen',
+        // buttons
+        'buttons' : buttons,
+        // path to button images (must end with a slash)
+        'buttonsImagePath' : '../greyskin/', 
+        // button groups
+        'buttonsStandard' : ["reference","zoomin","zoomout","zoomarea","zoomfull","pagewidth","back","fwd","page","bird","SEP","help","reset","options"],
+        'buttonsSpecial' : ["mark","delmark","hmir","vmir","rot","brgt","cont","rgb","quality","size","calibrationx","scale","SEP","options"],
+        'buttonsCustom' : []
+        };
+ 
+    // parameters from the query string
+    var queryParams = {};
+
+    // affine geometry classes
+    var geom = dlGeometry();
+    
+    var methods = {
+            // digilib initialization
+            init : function(options) {
+                // settings for this digilib instance are merged from defaults and options
+                var settings = $.extend({}, defaults, options);
+                var isFullscreen = settings.interactionMode === 'fullscreen'; 
+                if (isFullscreen) {
+                    queryParams = parseQueryParams();
+                    };
+                return this.each(function() {
+                    var $elem = $(this);
+                    var data = $elem.data('digilib');
+                    var elemSettings;
+                    // if the plugin hasn't been initialized yet
+                    if (!data) {
+                        // merge query parameters
+                        if (isFullscreen) {
+                            elemSettings = $.extend({}, settings, queryParams);
+                        } else {
+                            elemSettings = $.extend({}, settings, parseImgParams($elem));
+                        };
+                        // store in data element
+                        $elem.data('digilib', {
+                            target : $elem,
+                            settings : elemSettings
+                        });
+                    }
+                    // create HTML structure
+                    setupScalerDiv($elem, elemSettings);
+                    setupButtons($elem, elemSettings, 'buttonsStandard');
+                });
+            },
+
+            // clean up digilib
+            destroy : function() {
+                return this.each(function(){
+                    var $this = $(this);
+                    var data = $this.data('digilib');
+                    // Namespacing FTW
+                    $(window).unbind('.digilib'); // unbinds all digilibs(?)
+                    data.digilib.remove();
+                    $this.removeData('digilib');
+                });
+            }
+    };
+
+    // returns parameters from page url
+    var parseQueryParams = function() {
+        return parseQueryString(location.search.slice(1));
+        };
+        
+    // returns parameters taken from embedded img-element
+    var parseImgParams = function($elem) {
+        var src = $elem.find('img').first().attr('src');
+        if (!src) {
+            return null;
+        }
+        var pos = src.indexOf('?');
+        var query = (pos < 0) ? '' : src.substring(pos + 1);
+        var scalerUrl = src.substring(0, pos);
+        var hash = parseQueryString(query);
+        hash.scalerBaseUrl = scalerUrl;
+        // console.log(hash);
+        return hash;
+        };
+
+    // parses query parameter string into parameter object
+    var parseQueryString = function(query) {
+        var pairs = query.split("&");
+        var hash = {};
+        for (var i = 0; i < pairs.length; i++) {
+            var pair = pairs[i].split("=");
+            if (pair.length === 2) {
+                hash[pair[0]] = pair[1];
+                };
+            };
+        return hash;
+        };
+    
+    // returns URL and query string for Scaler
+    var getScalerString = function (settings) {
+        var url = settings.scalerBaseUrl + '?';
+        var i, parm, latter;
+        // go through param names and get values from settings
+        for (i = 0; i < settings.scalerParamNames.length; ++i) {
+            parm = settings.scalerParamNames[i];
+            if (settings[parm]) {
+                // first parm gets no '&'
+                url += latter ? '&' : '';
+                latter = 1;
+                // add parm=val
+                url += parm + '=' + settings[parm];
+            }
+        }
+        return url;
+    };
+    
+    // returns maximum size for scaler img in fullscreen mode
+    var getFullscreenImgSize = function($elem) {
+        var winH = $(window).height();
+        var winW = $(window).width();
+        // TODO: account for borders?
+        return geom.size(winW, winH);
+    };
+    
+    // creates HTML structure for digilib in elem
+    var setupScalerDiv = function ($elem, settings) {
+        var rewrite;
+        if (settings.interactionMode === 'fullscreen') {
+            // fullscreen
+            var imgSize = getFullscreenImgSize($elem);
+            settings.dw = imgSize.width;
+            settings.dh = imgSize.height;
+            $img = $('<img/>');
+            var scalerUrl = getScalerString(settings);
+            $img.attr('src', scalerUrl);
+            $img.addClass('pic');
+        } else {
+            // embedded mode -- try to keep img tag
+            var $img = $elem.find('img');
+            if ($img.length > 0) {
+                console.debug("img detach:",$img);
+                $img.detach();
+                $img.addClass('picsi');
+            } else {
+                $img = $('<img/>');
+                var scalerUrl = getScalerString(settings);
+                $img.attr('src', scalerUrl);
+                $img.addClass('pic');
+            }
+        }
+        // create new html
+        $elem.empty(); // TODO: should we keep stuff for customization?
+        var $scaler = $('<div class="scaler"/>');
+        $elem.append($scaler);
+        $scaler.append($img);
+        $img.load(scalerImgLoadedFn(settings));
+    };
+        
+    // creates HTML structure for buttons in elem
+    var setupButtons = function ($elem, settings, buttonGroup) {
+        if (settings.interactionMode === 'fullscreen') {
+            // fullscreen -- create new
+            var $buttonsDiv = $('<div class="buttons"></div>');
+            $elem.append($buttonsDiv);
+            var buttonNames = settings[buttonGroup];
+            for (var i = 0; i < buttonNames.length; i++) {
+                var buttonName = buttonNames[i];
+                var buttonSettings = settings.buttons[buttonName];
+                // construct the button html
+                var $button = $('<div class="button"></div>');
+                var $a = $('<a/>');
+                var $img = $('<img class="button"/>');
+                $buttonsDiv.append($button);
+                $button.append($a);
+                $a.append($img);
+                // add attributes and bindings
+                $button.attr('title', buttonSettings.tooltip);
+                $button.addClass('button-' + buttonName);
+                // let the clicked <a> element know about the digilib context 
+                $a.data('digilib', { 'name' : buttonName, 'settings' : settings } );
+                $a.bind('click', function() {
+                    // get the context settings
+                    var data = $(this).data('digilib');
+                    // find the action for the clicked element
+                    console.log(data.settings.buttons[data.name].onclick);
+                    });
+                // binding mit closure
+                //(function(){ var action = buttonSettings.onclick;
+                //    $a.bind('click', function(){ console.log( action )} );
+                //})();
+                $img.attr('src', settings.buttonsImagePath + buttonSettings.img);
+            };
+        }
+        return $buttonsDiv;
+    };
+    
+    // returns function for load event of scaler img
+    var scalerImgLoadedFn = function(settings) {
+        return function() {
+            console.debug("img loaded! settings=", settings);
+        };
+    };
+    // hook plugin into jquery
+    $.fn.digilib = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof(method) === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.digilib' );
+        }
+    };
+    
+})(jQuery);
---
+++ servlet/src/digilib/auth/AuthOps.java
@@ -0,0 +1,115 @@
+/*  AuthOps -- Authentication interface class
+
+  Digital Image Library servlet components
+
+  Copyright (C) 2001, 2002 Robert Casties (robcast@mail.berlios.de)
+
+  This program is free software; you can redistribute  it and/or modify it
+  under  the terms of  the GNU General  Public License as published by the
+  Free Software Foundation;  either version 2 of the  License, or (at your
+  option) any later version.
+   
+  Please read license.txt for the full details. A copy of the GPL
+  may be found at http://www.gnu.org/copyleft/lgpl.html
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+package digilib.auth;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import digilib.servlet.DigilibRequest;
+
+/** Class of operations requiring authentication. */
+public interface AuthOps {
+
+	/** Test if the request must be authorized to access the filepath.
+	 *
+	 * Information about the user is taken from the ServletRequest.
+	 * @param filepath filepath to be accessed.
+	 * @param request ServletRequest with user information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return true if the user request must be authorized.
+	 */
+	public boolean isAuthRequired(String filepath, HttpServletRequest request)
+		throws AuthOpException;
+
+	/** Test if the request must be authorized to access the filepath.
+	 *
+	 * Information about the user is taken from the DigilibRequest.
+	 * @param request DigilibRequest with user information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return true if the user request must be authorized.
+	 */
+	public boolean isAuthRequired(DigilibRequest request)
+		throws AuthOpException;
+
+	/** Test if the request is allowed to access filepath.
+	 * 
+	 * @param filepath filepath to be acessed.
+	 * @param request Request with user information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return true if the request is allowed.
+	 */
+	public boolean isAuthorized(String filepath, HttpServletRequest request)
+		throws AuthOpException;
+
+	/** Test if the request is allowed to access filepath.
+	 * 
+	 * @param request Request with user information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return true if the request is allowed.
+	 */
+	public boolean isAuthorized(DigilibRequest request)
+		throws AuthOpException;
+
+	/** Authorization roles needed for request.
+	 *
+	 * Returns the list of authorization roles that are needed to access the
+	 * specified path. No list means the path is free.
+	 *
+	 * The location information of the request is also considered.
+	 *
+	 * @param filepath filepath to be accessed.
+	 * @param request ServletRequest with address information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return List of Strings with role names.
+	 */
+	public List<String> rolesForPath(String filepath, HttpServletRequest request)
+		throws AuthOpException;
+
+	/** Authorization roles needed for request.
+	 *
+	 * Returns the list of authorization roles that are needed to access the
+	 * specified path. No list means the path is free.
+	 *
+	 * The location information of the request is also considered.
+	 *
+	 * @param request DigilibRequest with address information.
+	 * @throws AuthOpException Exception thrown on error.
+	 * @return List of Strings with role names.
+	 */
+	public List<String> rolesForPath(DigilibRequest request)
+		throws AuthOpException;
+
+	/** Test request authorization against a list of roles.
+	 * @param roles List of Strings with role names.
+	 * @param request ServletRequest with address information.
+	 * @return true if the user information in the request authorizes one of the roles.
+	 */
+	public boolean isRoleAuthorized(List<String> roles, HttpServletRequest request);
+
+	/** Test request authorization against a list of roles.
+	 * @param roles List of Strings with role names.
+	 * @param request ServletRequest with address information.
+	 * @return true if the user information in the request authorizes one of the roles.
+	 */
+	public boolean isRoleAuthorized(List<String> roles, DigilibRequest request);
+
+}
